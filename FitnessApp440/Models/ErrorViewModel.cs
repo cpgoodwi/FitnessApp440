f94aa973a96d4468db4324ab32135c81b3a9b69b
CharleyIsCool <cpgoodwi@asu.edu>
2022-09-18 14:26:28 -0700

    Add project files.

---
+++ FitnessApp440/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessApp440.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
