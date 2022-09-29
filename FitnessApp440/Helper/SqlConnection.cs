8c67d0d66f79a4ace11a1c02b4a9934b28b2c832
mikaelstorm50 <MIkaelstorm50@gmail.com>
2022-09-29 05:35:48 -0700

    I created a Food model, Form Validation, and a Sql connection class

---
+++ FitnessApp440/Helper/SqlConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace FitnessApp440.Helper
+{
+
+    // This class is connections to the SQL Database
+    public static class SqlConnection
+    {
+        private static MySqlConnection connection;
+        private static MySqlCommand cmd = null;
+        private static DataTable dt;
+        private static MySqlDataAdapter sdm;
+
+        public static void EstablishConnection()
+        {
+             String ConnectionResult;
+
+            try
+            {
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "107.180.1.16";
+                builder.UserID = "440fall20224";
+                builder.Password = "440fall20224";
+                builder.Database = "440fall20224";
+                builder.SslMode = MySqlSslMode.None;
+                connection = new MySqlConnection(builder.ToString());
+                ConnectionResult = " Database Connectioi Successful";
+
+            }
+            catch(Exception ex)
+            {
+                ConnectionResult = "Connection Failed";
+            }
+        }
+    }
+}
+
---
+++ FitnessApp440/Validations/Validation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using static FitnessApp440.Models.UserViewModel;
+namespace FitnessApp440.Validations
+{
+    // This is a validation class that will be used to validate the entry from the user.
+    public class Validation
+    {
+        // This method checks to see if the ussername has 20 characters
+        public bool IsUserNameLengthValid(string password)
+        {
+            return String.IsNullOrEmpty(password) || password.Length <= 20;
+
+        }
+
+        // This method checks to see if the ussername has 100 characters
+        public bool IsImageStringValid(string password)
+        {
+            return String.IsNullOrEmpty(password) || password.Length <= 100;
+
+        }
+
+        // This method checks to see if the password has 40 characters
+        public bool IsPasswordLengthValid(string password)
+        {
+            return String.IsNullOrEmpty(password) || password.Length <= 40;
+
+        }
+
+        // This method checks to see if the text text are strings. This validation can be used multiples times
+        public bool IsTextValid(string text)
+        {
+            return String.IsNullOrEmpty(text);
+
+        }
+
+        // This method is a regex that validates the passwords the user inputs.
+        public bool IsPasswordValid(string password)
+        {
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMinimum8Chars = new Regex(@".{8,}");
+
+            var isValidated = hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum8Chars.IsMatch(password);
+
+            return true;
+        }
+        
+    }
+}
+
