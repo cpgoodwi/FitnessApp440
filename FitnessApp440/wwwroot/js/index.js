4a20d6d83ba69d5410306971490b2c4e27eb8ced
cpgoodwi <cpgoodwi@asu.edu>
2022-09-29 15:19:18 -0700

    formated Food.js and added testing code to Index view. Also added TODO file to list things to do

---
+++ FitnessApp440/wwwroot/js/index.js
@@ -0,0 +1,7 @@
+﻿let foodList = []
+
+foodList.push(new Food(0001, "hot dog", 100, 100, 100, 100, "hot dog", "This is a hot dog"))
+
+for (const food of foodList) {
+    $("#food-feed").append(food.renderHTML())
+}
